e7ab4ec1ebb08a7f582a16898a0a4fde50a37a03
Madeeha <khanm57@mcmaster.ca>
2018-12-01 17:37:01 -0500

    fixed index.php

--- index.php
+++ index.php
@@ -55,6 +55,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     
     // Validate credentials
+     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username = :username";
         
